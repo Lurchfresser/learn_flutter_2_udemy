ddacdff5ab0d1cea7b95cc4ab9278524187c29a7
TMeyne <TMeyne@huth.org>
2023-11-28 13:05:40 +0100

    Implemented Failures (Video 78)

--- lib/2_application/pages/advice/cubit/advicer_cubit.dart
+++ lib/2_application/pages/advice/cubit/advicer_cubit.dart
@@ -1,9 +1,16 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:learn_flutter_2_udemy/1_domain/entities/advice_entity.dart';
+import 'package:learn_flutter_2_udemy/1_domain/failures/failures.dart';
 import 'package:learn_flutter_2_udemy/1_domain/usecases/advice_usecases.dart';
 
 part 'advicer_state.dart';
+
+
+//maybe exclude this later
+const serverFailureMessage = "Server Failure";
+const cacheFailureMessage = "Cache Failure";
+const generalFailureMessage = "OOps, something went wrong";
 
 class AdvicerCubit extends Cubit<AdvicerCubitState> {
   AdvicerCubit() : super(AdvicerInitial());
@@ -13,8 +20,22 @@ class AdvicerCubit extends Cubit<AdvicerCubitState> {
   void adviceRequested() async {
     emit(AdvicerStateLoading());
     //business logic
-    final AdviceEntity advice = await adviceUsecases.getAdvice();
-    //emit(const AdvicerStateError(message: "Error"));
-    emit(AdvicerStateLoaded(advice: advice.advice));
+    final  failurOrAdvice = await adviceUsecases.getAdvice();
+    failurOrAdvice.fold(
+      (failure) => emit(AdvicerStateError(message: _mapFailureToMessage(failure))),
+      (advice) => emit(AdvicerStateLoaded(advice: advice.advice)),
+    );
+  }
+
+  //maybe exclude this later
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case const (ServerFailure):
+        return serverFailureMessage;
+      case const (CacheFailure):
+        return cacheFailureMessage;
+      default:
+        return generalFailureMessage;
+    }
   }
 }
